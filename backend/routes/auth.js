788930a218308841eb38306a88a35bb033d43a85
shaikhusama155 <shaikhusama155@gmail.com>
2024-01-05 20:41:05 +0500

    add

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,38 +1,54 @@
 const express = require("express");
 const User = require("../Models/User");
 const router = express.Router();
-const { body, validationResult } = require('express-validator');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const jwt_Secret = "yehnhbatasakta";
+const { body, validationResult } = require("express-validator");
 
 // Create a user using: POST "/api/auth/createuser". Doesn't require Auth
-router.post("/createuser", [
-  body('name', "Enter Your Name").isLength({ min: 3 }),
-  body('password', "Enter a Password").isLength({ min: 5 }),
-  body('email', "Enter a valid Email").isEmail(),
-], async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-//check wether the email already exists
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter Your Name").isLength({ min: 3 }),
+    body("password", "Enter a Password").isLength({ min: 5 }),
+    body("email", "Enter a valid Email").isEmail(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    //check wether the email already exists
 
-try {
-  
-let user = await User.findOne({email: req.body.email})
-if(user){
-  return res.status(400).json({error: "Sorry this email is already exists"})
-}
- user = await User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  }) 
-  res.json({"nice":"nice"});
-} catch (error) {
-  console.error(error.massage)
-  res.status(500).json("Some Error over here")
-}
-  });
-  
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry this email is already exists" });
+      }
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+      });
+      const data = {
+        user: {
+          id : user.id,
+        },
+      };
+      const authToken = jwt.sign(data, jwt_Secret);
+      res.json({authToken});
+    } catch (error) {
+      console.error(error.massage);
+      res.status(500).json("Some Error over here");
+    }
+  }
+);
+
 //   .then(user => res.json(user))
 //   .catch(err=> {console.log(err)
 //   res.json({error: 'Please Enter a Unique Email'})} )
